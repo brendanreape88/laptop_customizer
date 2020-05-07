aac996fd507c41da0c9127c5520091b4efb55aa4
Brendan Reape <brendanreape88@gmail.com>
2020-05-07 14:12:23 -0700

    components finished

--- src/Cart.js
+++ src/Cart.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import CartTotal from './CartTotal'
 
 class Cart extends React.Component {
     render() {
@@ -16,23 +17,9 @@ class Cart extends React.Component {
               </div>
             );
           });
-      
-          const total = Object.keys(this.state.selected).reduce(
-            (acc, curr) => acc + this.state.selected[curr].cost,
-            0
-          );
 
         return (
-            <section className="main__summary">
-                <h2>Your cart</h2>
-                {summary}
-                <div className="summary__total">
-                <div className="summary__total__label">Total</div>
-                <div className="summary__total__value">
-                    {USCurrencyFormat.format(total)}
-                </div>
-                </div>
-            </section>
+            <CartTotal />
         )
 
     }
